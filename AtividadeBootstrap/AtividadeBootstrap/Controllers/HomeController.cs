82edbf914610114f89ac47653ff5dba0d72d2b8e
Carlos Andrew Costa Bezerra <andrew@r2asistemas.com.br>
2019-11-29 20:22:03 -0300

    Adicionando arquivos do Projeto

---
+++ AtividadeBootstrap/AtividadeBootstrap/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AtividadeBootstrap.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtividadeBootstrap.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Produto> lista = new List<Produto> {
+                new Produto{ID=1,Descricao="Detergente Liquido",Preco=1.59},
+                new Produto{ID=2,Descricao="Sabao em Pó",Preco=9.59},
+                new Produto{ID=3,Descricao="Limpa Inox",Preco=4.59},
+
+            };
+            return View(lista);
+        }
+    }
+}
---
+++ AtividadeBootstrap/AtividadeBootstrap/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AtividadeBootstrap.Models
+{
+    public class Produto
+    {
+        public int ID { get; set; }
+        public string Descricao { get; set; }
+        public double Preco { get; set; }
+    }
+}
